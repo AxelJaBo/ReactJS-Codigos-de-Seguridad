8f00a7c4bc0d19986bbde9536d8a188f54dffc8c
AxelJaBo <axel.910921@gmail.com>
2023-11-24 20:56:57 -0800

    ¿Dónde actualizar el estado?

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -12,8 +12,9 @@ function UseState({ name }){
         if(!!loading){
             setTimeout(() => {
                 console.log("Haciendo la validación");
-                if(value !== SECURITY_CODE){
+                if(value === SECURITY_CODE){
                     setLoading(false);
+                    // setError(false)
                 }else{
                     setError(true);
                     setLoading(false);
@@ -28,7 +29,7 @@ function UseState({ name }){
         <div>
             <h2>Eliminar {name}</h2>
             <p>Por favor, escribe el código de seguridad.</p>
-            {error && (
+            {(error && !loading) && (
                 <p>Error: el código es incorrecto.</p>
             )}
             {loading && (

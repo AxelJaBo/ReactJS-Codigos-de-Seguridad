1f467f64768e57b77e790377ce5d4693eb3ffcad
AxelJaBo <axel.910921@gmail.com>
2023-11-26 20:14:41 -0800

    Estados compuestos con React.Component

--- src/ClassState.jsx
+++ src/ClassState.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Loading } from "./Loading";
+
+const SECURITY_CODE = 'paradigma';
 class ClassState extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
+            value: '',
             error: false,
             loading: false,
         };
@@ -25,7 +28,11 @@ class ClassState extends React.Component{
         if(!!this.state.loading){
             setTimeout(() => {
                 console.log("Haciendo la validación");
-                this.setState({ loading: false})
+                if(SECURITY_CODE === this.state.value){
+                    this.setState({ error: false, loading: false });
+                }else {
+                    this.setState({ error: true, loading: false });
+                }
                 console.log("Terminando la validación");
             }, 3000);
         }
@@ -36,14 +43,21 @@ class ClassState extends React.Component{
             <div>
                 <h2>Eliminar {this.props.name}</h2>
                 <p>Por favor, escribe el código de seguridad.</p>
-                {this.state.error && (
+                {(this.state.error && !this.state.loading) && (
                     <p>Error: el código es incorrecto.</p>
                 )}
 
                 {this.state.loading && (
                     <Loading/>
                 )}
-                <input placeholder="Código de seguridad" type="text" />
+                <input 
+                    placeholder="Código de seguridad" 
+                    type="text" 
+                    value={this.state.value}
+                    onChange={(event) => {
+                        this.setState({ value: event.target.value })
+                    }}
+                />
                 <button
                     onClick={() => this.setState({ loading: true})}
                 >
